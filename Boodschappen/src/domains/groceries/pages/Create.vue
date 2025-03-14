01f608eb0f212960eba262e64b8642ea6621d990
ProgramMaarten <wiepwiep10@gmail.com>
2025-03-14 10:51:43 +0100

    Update Create.vue

--- Boodschappen/src/domains/groceries/pages/Create.vue
+++ Boodschappen/src/domains/groceries/pages/Create.vue
@@ -1,6 +1,6 @@
 <template>
     <GroceryForm :grocery="grocery" @updateMessage="handleMessage" />
-    <p>Bericht van kindcomponent: {{ message }}</p>
+    
 </template>
 
 <script setup>
@@ -11,11 +11,10 @@ import GroceryForm from './../components/GroceryForm.vue';
 import {addGrocery} from './../store.js';
 
 const grocery = ref({product: '', price: 1, number: 1});
-const message = ref('');
+
 
 const handleMessage = newMessage => {
-    message.value = newMessage;
-    console.log(newMessage);
+
     addGrocery(newMessage);
 };
 </script>
